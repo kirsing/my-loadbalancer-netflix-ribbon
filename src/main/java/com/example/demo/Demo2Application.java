f777107af2014bb3895322d19fa078fe99f93030
kirsing <kirsing98@icloud.com>
2022-08-22 00:10:29 +0300

    init

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+
+@RibbonClient(name = "ping-a-server", configuration = RibbonConfiguration.class)
+@EnableEurekaClient
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+}
